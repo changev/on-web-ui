d4a2e6eba08756bc69c355940717d6e64aea8ef3
changev <alan.wei@emc.com>
2017-08-08 19:57:30 +0800

    f

--- pipeline/SprintRelease/Start.groovy
+++ pipeline/SprintRelease/Start.groovy
@@ -1,7 +1,17 @@
-def LIB_BRANCH = "feature/web-ui-release-lib"
-library "rackhd-pipline-lib@$LIB_BRANCH"
-
-node {
-    checkout scm
-    load('./pipeline/SprintRelease/SprintRelease')
+if (env.LIB_VERSION == null || env.LIB_VERSION == ""){
+    env.LIB_VERSION="master"
 }
+if (env.LIB_URL == null || env.LIB_URL == ""){
+    env.LIB_URL="https://github.com/rackhd/on-build-config"
+}
+if (env.LIB_NAME == null || env.LIB_NAME == ""){
+    env.LIB_NAME="rackhd-devel-lib"
+}
+
+library env.LIB_NAME
+
+node{
+    checkout scm
+    load('pipeline/SprintRelease/SprintRelease')
+}
+
